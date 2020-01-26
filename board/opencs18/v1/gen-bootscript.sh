b7ff4ce48bf6b8a8579d209d38826b28a832c56d
Roman Savrulin <romansavrulin@gmail.com>
2020-01-26 13:58:02 +0300

    build dev and prd boot scripts

--- board/opencs18/v1/gen-bootscript.sh
+++ board/opencs18/v1/gen-bootscript.sh
@@ -3,6 +3,9 @@
 SRC_SCRIPT=${BR2_EXTERNAL_OPENCS18_PATH}/board/opencs18/v1/usb-bootscript.raw
 DST_IMG=${BINARIES_DIR}/recovery.scr
 
+SRC_SCRIPT=${1:-$SRC_SCRIPT}
+DST_IMG=${2:-$DST_IMG}
+
 #env
 
 echo Generating $DST_IMG
--- board/opencs18/v1/post-build.sh
+++ board/opencs18/v1/post-build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# build production startup script
 ${DIR}/gen-bootscript.sh
+
+# build dev startup script
+${DIR}/gen-bootscript.sh ${BR2_EXTERNAL_OPENCS18_PATH}/board/opencs18/v1/dev-usb-bootscript.raw ${BINARIES_DIR}/dev-recovery.scr
 
 #grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 #	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
