b1011d47559f6e1e78b0992d9c43c4e6d1c2445f
Roman Savrulin <romansavrulin@gmail.com>
2020-02-04 00:27:26 +0300

    Sucessful boot from NFS

--- board/opencs18/v1/post-build.sh
+++ board/opencs18/v1/post-build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# build production startup script
+SCRIPT_PATH=${BR2_EXTERNAL_OPENCS18_PATH}/board/opencs18/v1
+
+echo Building production startup script
 ${DIR}/gen-bootscript.sh
 
-# build dev startup script
-${DIR}/gen-bootscript.sh ${BR2_EXTERNAL_OPENCS18_PATH}/board/opencs18/v1/dev-usb-bootscript.raw ${BINARIES_DIR}/dev-recovery.scr
+echo Building dev startup script
+${DIR}/gen-bootscript.sh ${SCRIPT_PATH}/dev-usb-bootscript.raw ${BINARIES_DIR}/dev-recovery.scr
+
+echo Building NFS startup script
+
+# the following substitution doesn't work yet. #ip and nfsroot is hardcoded
+cp $SCRIPT_PATH/nfs-bootscript.tmpl $SCRIPT_PATH/nfs-bootscript.raw
+sed -i "s/<ip>/${BR2_PACKAGE_OPENCS18_NFS_TARGET_IP_ADDRESS}/g" $SCRIPT_PATH/nfs-bootscript.raw
+sed -i "s/<nfsroot>/${BR2_PACKAGE_OPENCS18_NFS_TARGET_IP_ADDRESS}/g" $SCRIPT_PATH/nfs-bootscript.raw
+${DIR}/gen-bootscript.sh ${SCRIPT_PATH}/nfs-bootscript.raw ${BINARIES_DIR}/nfs-recovery.scr
 
 #grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 #	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
